f519a58e74a21a92d0f119a0cbde96a4026f554a
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-01 14:35:03 +0300

    Product schema finished

---
+++ E-Commerce-standalone/src/app/models/iproducts.ts
@@ -0,0 +1,10 @@
+export interface Iproducts {
+  productId: number;
+  productName: string;
+  productImgUrl: string;
+  productQuantity: number;
+  productPrice: number;
+  categoryId: number;
+  productDetails: string;
+  showImage:boolean;
+}
