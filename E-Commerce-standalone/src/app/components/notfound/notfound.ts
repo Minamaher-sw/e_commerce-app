0b14797297256ebb9abc3ac9c56245dc96a53979
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-04 03:30:22 +0300

    not found component is finished

---
+++ E-Commerce-standalone/src/app/components/notfound/notfound.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterEvent, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-notfound',
+  imports: [RouterModule , RouterLink],
+  templateUrl: './notfound.html',
+  styleUrl: './notfound.css'
+})
+export class Notfound {
+
+}
