83f959d50624dfa5d523b121efcf47a65ceb7946
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-04 09:23:13 +0300

    importing for navbar , header,product-parent and footer components

--- E-Commerce-standalone/src/app/components/home/home.ts
+++ E-Commerce-standalone/src/app/components/home/home.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { Header } from "../header/header";
+import { ProductParent } from "../product-parent/product-parent";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Navbar, Header, ProductParent, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
