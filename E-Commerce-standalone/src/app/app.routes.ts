ebf75bb390576a6541520fcbd201732f27325bab
Minamaher-sw <mosadefmena185@gmail.com>
2025-06-14 15:32:16 +0300

    feat: add route for TemplateDrivenSignup component

--- E-Commerce-standalone/src/app/app.routes.ts
+++ E-Commerce-standalone/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { Aboutus } from './components/aboutus/aboutus';
 import { ProductParent } from './components/product-parent/product-parent';
 import { Maincomponent } from './components/maincomponent/maincomponent';
 import { ProductDetails } from './components/product-details/product-details';
+import { TemplateDrivenSignup } from './components/template-driven-signup/template-driven-signup';
 
 export const routes: Routes = [
   {
@@ -16,7 +17,7 @@ export const routes: Routes = [
       { path: "aboutus", component: Aboutus, title: "Home Page" },
       { path: "products", component: ProductParent, title: "Home Page" },
       {path:"product-parent/:id",component:ProductDetails},
-
+      {path:"signup" ,component:TemplateDrivenSignup}
     ]
   },
   { path: "**", component: Notfound, title: "not found" }
